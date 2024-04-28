1de1d30df2361fccfa34b383f9f7b2593edaf4ab
Jason Hargrove <rijaha@gmail.com>
2024-04-27 19:23:03 -0500

    added computed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
         </tr>
       </thead>
       <tbody>
-        <tr v-for="artist in artists" :key="artist.name">
+        <tr v-for="artist in filteredArtists" :key="artist.name">
           <td>{{ artist.name }}</td>
           <td>{{ artist.phone }}</td>
           <td>{{ artist.address }}</td>
@@ -27,6 +27,14 @@ export default {
     return {
       artists: [],
     }
+  },
+  computed: {
+    filteredArtists() {
+      return this.artists.filter((artist) => {
+        const addressUpper = artist.address.toUpperCase()
+        return addressUpper.endsWith('ES') || addressUpper.endsWith('IT')
+      })
+    },
   },
   mounted() {
     axios
